48775572865728600187e39d4eab5de65ca58f0c
aaxlss <axl9305@gmail.com>
2022-11-29 15:35:37 +0000

    [Update] Using a single state to update the value of the another states [Update] Creating new methods to update the states

--- src/UseState/index.js
+++ src/UseState/index.js
@@ -1,44 +1,108 @@
-import React, { useEffect, useState } from "react";
+import React, { Fragment, useEffect, useState } from "react";
 
-const SECURITY_CODE = 'paradigma'
+const SECURITY_CODE = "paradigma";
 
-function UseState ({name}){
-    const [error, setError] = useState(false);
-    const [value, setValue] = useState('');
-    const [loading, setLoading] = useState(false);
+function UseState({ name }) {
+  const [state, setState] = useState({
+    error: false,
+    value: "",
+    loading: false,
+    deleted: false,
+    confirmed: false,
+  });
 
-    useEffect(() => {
-        console.log('iniciando effecto');
-        if(!!loading){
-            setTimeout(()=>{
-                console.log('haciendo validacion');
-                if (value !== SECURITY_CODE){
-                    setError(true);   
-                }
-                setLoading(false)
-                console.log('Terminando validacion');
-            },1500)
+  useEffect(() => {
+    console.log("iniciando effecto");
+    if (!!state.loading) {
+      setTimeout(() => {
+        console.log("haciendo validacion");
+        if (state.value !== SECURITY_CODE) {
+          console.log("error validation");
+          setState({
+            ...state,
+            error: true,
+            loading: false,
+          });
+        } else {
+          setState({
+            ...state,
+            confirmed: true,
+          });
         }
-        
-        console.log('terminando effecto');
-    }, [loading]);
+      }, 1500);
+    }
+
+    console.log("terminando effecto");
+  }, [state.loading]);
+
+  const onSetValue = (newValue) => {
+    setState({
+      ...state,
+      value: newValue,
+    });
+  };
+  const onValitateValue = () => {
+    setState({
+      ...state,
+      loading: true,
+      error: false,
+    });
+  };
+  const onDelete = () => {
+    setState({
+      ...state,
+      deleted: true,
+      confirmed: true,
+    });
+  };
+
+  const onRevert = () => {
+    setState({
+      ...state,
+      deleted: false,
+      confirmed: false,
+      loading: false,
+    });
+  };
+  if (!state.deleted && !state.confirmed) {
     return (
-        <div>
-            <h2>Eliminar {name}</h2>
-            <p>Ingresa el codigo de seguridad</p>
-            {error && <p>Error: codigo incorrecto</p>}
-            {loading && <p>Loading...</p>}
-            <input 
-            placeholder="Codigo de seguridad"
-            value={value}
-            onChange={(event) =>setValue( event.target.value) }
-            />
-            <button onClick={() => {
-                setLoading(true);
-                setError(false);
-                }}>Comprobar</button>
-        </div>
-    )
+      <div>
+        <h2>Eliminar {name}</h2>
+        <p>Ingresa el codigo de seguridad</p>
+        {state.error && <p>Error: codigo incorrecto</p>}
+        {state.loading && <p>Loading...</p>}
+        <input
+          placeholder="Codigo de seguridad"
+          value={state.value}
+          onChange={(event) => {
+            onSetValue(event.target.value);
+          }}
+        />
+        <button
+          onClick={() => {
+            onValitateValue();
+          }}
+        >
+          Comprobar
+        </button>
+      </div>
+    );
+  } else if (!state.deleted && !!state.confirmed) {
+    return (
+      <Fragment>
+        <p>Confirm screen</p>
+        <button onClick={() => onDelete()}>Delete</button>
+        <button onClick={() => onRevert()}>Cancel</button>
+      </Fragment>
+    );
+  } else if (!!state.deleted && !!state.confirmed) {
+    return (
+      <Fragment>
+        <p>Successful deletion</p>
+        <button onClick={() => onRevert()}>Restart the values</button>
+      </Fragment>
+    );
+  }
 }
 
-export {UseState}
+export { UseState };
